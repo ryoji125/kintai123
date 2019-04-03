dc60b0498bf4052ec25034c92899824441d05622
yoshida <nyarun115@gmail.com>
2019-04-03 11:06:58 +0000

    loginform

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -8,10 +10,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "アカウントを作成しました。"
       redirect_to @user
     else
       render "new"
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "更新に成功しました"
+      redirect_to @user
+    else
+      render "edit"
     end
   end
   
@@ -21,4 +38,17 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
     
+    #beforeフィルター
+    
+    def logged_in_user
+      unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless @user == current_user
+    end
 end
