33effa1074ae21207d0a5489cccdc96a17b64017
yoshida <nyarun115@gmail.com>
2019-05-01 02:41:56 +0000

    attendance-edit-page

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,11 +1,11 @@
 class Attendance < ApplicationRecord
   belongs_to :user
-  validate :started_at_none
-  
-
+    validate :started_at_none
+    
 def started_at_none
-    if started_at.nil? 
-        errors.add(:finished_at, ":は出勤時間が無いため登録できません。")
+    if  started_at.nil? 
+        errors.add(:finished_at, "退社時間は出勤時間が無いため登録できません。")
     end
 end
+
 end
