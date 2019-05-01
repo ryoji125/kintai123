66b3f4e83150c0626a513bedbad50a19a4d6f170
yoshida <nyarun115@gmail.com>
2019-05-01 03:04:58 +0000

    master

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,8 +3,11 @@ class Attendance < ApplicationRecord
     validate :started_at_none
     
 def started_at_none
-    if  started_at.nil? 
+    if  worked_on <= Date.today && started_at.nil? 
         errors.add(:finished_at, "退社時間は出勤時間が無いため登録できません。")
+    elsif worked_on > Date.today
+        errors.add(:finished_at, "退社時間は出勤時間が無いため登録できません。")
+    else
     end
 end
 
